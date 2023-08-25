1457c872a5ef290bbec74e80c1f6c6aeb5a4771e
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2023-08-25 16:26:23 -0500

    Add acquire program that handles devices' image data ready events
    Basic structure of the acquire program. Subscribes to data ready events
    for given list of cameras and provides a to-be-written callback.
    
    Includes command line interface where devices can be specified.

---
+++ DAQClient/daqclient/acquire.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import argparse
+
+from tango import AttributeProxy, EventType, DataReadyEventData
+
+class Acquirer:
+    def __init__(self, device_list: list[str]):
+        """
+        Parameters
+        ----------
+        device_list : list[str]
+            list of Tango handles that this Client will subscribe to
+        """
+
+        self.image_attributes = [
+            AttributeProxy(f"{device}/image")
+            for device in device_list
+        ]
+
+        # subscribe to the image attributes' data ready events
+        for image_attr in self.image_attributes:
+            image_attr.subscribe_event(EventType.DATA_READY_EVENT, self.handle_data_ready_event)
+
+    def handle_data_ready_event(self, event: DataReadyEventData):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(
+        usage="python -m daqclient.acquire device1[, device2[, device3 ...]]",
+        description="Listens to events and passes them on to processing.",
+    )
+    argparser.add_argument("device_list", nargs="+", help="One or more Tango device handles")
+    args = argparser.parse_args()
+
+    acquirer = Acquirer(args.device_list)
+    acquirer.run()
+
