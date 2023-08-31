c1141c4fc52e0dd1e4b37cafbdcc0b47a50826b2
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2023-08-30 19:01:59 -0500

    Handle TimingUnit ShotID change event in DAQClient
    * subscribe to change event
    * add shot_number, and handler to update it
    * save images with shot_number from timing unit
    * add command line arguments for timing device

--- DAQClient/daqclient/acquire.py
+++ DAQClient/daqclient/acquire.py
@@ -5,14 +5,14 @@ from pathlib import Path
 from typing import Optional, TYPE_CHECKING
 if TYPE_CHECKING:
     import numpy as np
-    from tango import DataReadyEventData
+    from tango import DataReadyEventData, EventData
 from typing import NamedTuple
 
 from tango import AttributeProxy, EventType
 
 import imageio
 
-from .logging import get_logger
+from daqclient.logging import get_logger
 logger = get_logger('acquire')
 
 TANGO_HOST = "tango://localhost:10000"
@@ -22,19 +22,23 @@ class DeviceImageAcquireProperties(NamedTuple):
     save_folder: Optional[Path] = None
 
 class Acquirer:
-    def __init__(self, device_handles: list[str], save_folder: Optional[Path] = None):
+    def __init__(self, device_handles: list[str], timing_unit_device_handle: str, save_folder: Optional[Path] = None):
         """
         Parameters
         ----------
         device_list : list[str]
             list of Tango handles that this Client will subscribe to
+        timing_unit_device_handle : str
+            Tango handle of the TimingUnit device that triggers the cameras
         save_folder : str
             Top level folder for all devices' images. If not given, don't save images.
             Images are saved in <save_folder>/<device_handle>/0123.tiff
         """
 
         self.device_image_acquire_properties = {}
+        self.shot_number: int = 0
 
+        # subscribe to device image Data Ready Events
         for device_handle in device_handles:
             
             self.device_image_acquire_properties[device_handle] = DeviceImageAcquireProperties(
@@ -46,30 +50,52 @@ class Acquirer:
                 self.device_image_acquire_properties[device_handle].save_folder = save_folder / Path(device_handle)
                 self.device_image_acquire_properties[device_handle].save_folder.mkdirs(exist_ok=True, parents=True)
 
-            # subscribe to the image attributes' data ready events
-            self.device_image_acquire_properties[device_handle].attribute_proxy.subscribe_event(EventType.DATA_READY_EVENT, self.handle_data_ready_event)
+            # subscribe to the image attribute's data ready events
+            self.device_image_acquire_properties[device_handle].attribute_proxy.subscribe_event(EventType.DATA_READY_EVENT, self.handle_image_data_ready_event)
             logger.info("Subscribed to events from %(device)/%(attr)", 
                         {'device': device_handle, 
                          'attr': self.device_image_acquire_properties[device_handle].attribute_proxy.name()
                         }
                        )
 
-    def handle_data_ready_event(self, event: DataReadyEventData):
+        # subscribe to TimingUnit Change event
+        self.shot_id_attribute_proxy = AttributeProxy(f"{TANGO_HOST}/{timing_unit_device_handle}/ShotID")
+        self.shot_id_attribute_proxy.subscribe_event(EventType.CHANGE_EVENT, self.handle_shot_id_change_event)
+
+    def handle_image_data_ready_event(self, event: DataReadyEventData):
         """
+        Acquire data from the device who sent the Attribute DataReadyEvent, and
+        save it to disk.
+        
         Parameters
         ----------
         event : DataReadyEventData
             DataReady event data
         """
         device_handle = event.device.dev_name()
-        logger.info(f"Received data ready event from {device_handle}")
+        logger.info(f"Received data ready event (type {event.__class__}) from {device_handle}")
         
         # get image from device
         image: np.ndarray = event.device.read_attribute(event.attr_name)
         logger.info(f"Received image with shape {image.shape}")
 
         if self.device_image_acquire_properties[device_handle].save_folder:
-            imageio.imwrite(self.device_image_acquire_properties[device_handle].save_folder / f"{event.ctr:04d}.tiff", image)
+            imageio.imwrite(self.device_image_acquire_properties[device_handle].save_folder / f"{self.shot_number:04d}.tiff", image)
+
+
+    def handle_shot_id_change_event(self, event: EventData):
+        """
+        Update shot_number.
+        
+        Parameters
+        ----------
+        event : EventData
+            Change event data
+        """        
+        device_handle = event.device.dev_name()
+        logger.info(f"Received change event (type {event.__class__}) from {device_handle}")
+
+        self.shot_number = event.attr_value.value
 
     def run(self):
         while True:
@@ -78,11 +104,21 @@ class Acquirer:
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(
-        usage="python -m daqclient.acquire device1[, device2[, device3 ...]] [--save-folder /path/to/folder]",
         description="Listens to events and passes them on to processing.",
     )
-    argparser.add_argument("device_list", nargs="+", help="One or more Tango device handles")
-    argparser.add_argument("--save-folder", dest="save_folder", help="Top level folder for all devices' images. If not given, don't save images.", type=Path)
+    argparser.add_argument("--device-list", dest='device_list', nargs="+", 
+                           help="One or more Tango device handles", required=True, 
+                           metavar='device'
+                          )
+    argparser.add_argument("--timing-device", dest='timing_device', 
+                           help="Tango handle of the TimingUnit device.", 
+                           required=True, metavar='timingdevice'
+                          )
+    argparser.add_argument("--save-folder", dest="save_folder", 
+                           help="Top level folder for all devices' images. If "
+                                "not given, don't save images.", 
+                           type=Path, metavar="/path/to/folder"
+                          )
     args = argparser.parse_args()
 
     acquirer = Acquirer(args.device_list, args.save_folder)
