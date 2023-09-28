104c303139b539bed8076fcef8298898afaaba56
Dung D. Phan <brianp.dung@utexas.edu>
2023-09-27 20:12:43 -0500

    Update main.cpp

--- RunControl/main.cpp
+++ RunControl/main.cpp
@@ -12,16 +12,16 @@ public:
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    std::shared_ptr<Tango::DeviceProxy> TimingUnit = std::make_shared<Tango::DeviceProxy>("ut3/beamline/timingUnit");
+    std::shared_ptr<Tango::DeviceProxy> TimingUnitTest = std::make_shared<Tango::DeviceProxy>("ut3/beamline/timingUnitTest");
     auto callback = new TriggerCallback();
-    TimingUnit->subscribe_event("Timestamp", Tango::DATA_READY_EVENT, callback);
+    TimingUnitTest->subscribe_event("Timestamp", Tango::DATA_READY_EVENT, callback);
     std::shared_ptr<Tango::DeviceProxy> TopViewCamera = std::make_shared<Tango::DeviceProxy>("ut3/beamline/topview");
     std::shared_ptr<Tango::DeviceProxy> PointingCamera = std::make_shared<Tango::DeviceProxy>("ut3/beamline/pointing");
     std::shared_ptr<Tango::DeviceProxy> ElectronSpectrometerA = std::make_shared<Tango::DeviceProxy>("ut3/beamline/especA");
     std::shared_ptr<Tango::DeviceProxy> ElectronSpectrometerB = std::make_shared<Tango::DeviceProxy>("ut3/beamline/especB");
 
     MainWindow w;
-    w.setDeviceProxy_TimingUnit(TimingUnit);
+    w.setDeviceProxy_TimingUnit(TimingUnitTest);
     w.setDeviceProxy_TopViewCamera(TopViewCamera);
     w.setDeviceProxy_PointingCamera(PointingCamera);
     w.setDeviceProxy_ElectronSpectrometerA(ElectronSpectrometerA);
